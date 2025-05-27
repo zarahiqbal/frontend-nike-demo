f09e2527cc52859cd3149d43fe57225e21b4054c
zarahiqbal <zarahiiqbal@gmail.com>
2025-05-27 10:01:08 +0500

    page fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,18 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        // Nike brand colors
-        'nike-orange': '#ff6900',
-        'nike-black': '#111111',
-        'nike-gray': '#7e7e7e',
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
-} satisfies Config
+  plugins: [],
+} satisfies Config;
